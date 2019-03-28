4a391dc3baac4d6bc208e9cb3a240910d2f4a04f
sinslu <xsl19882008@126.com>
2019-03-28 11:59:40 +0800

    spring boot

--- src/main/java/com/xin/APP.java
+++ src/main/java/com/xin/APP.java
@@ -13,11 +13,16 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
         @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
         public class MyConfiguration {
         }
-
         The @SpringBootApplication annotation is equivalent to using
         @Configuration
         @EnableAutoConfiguration
         @ComponentScan with their default attributes
+
+        ApplicationRunner CommandLineRunner
+        SpringApplication.run(…)执行完后会调用这两个接口的run方法
+
+        通过实现org.springframework.core.Ordered接口或者
+        org.springframework.core.annotation.Order注解可以对bean初始化排序
  */
 @SpringBootApplication
 public class APP  extends SpringBootServletInitializer{
---
+++ src/main/java/com/xin/config/SpringMVCConfig.java
@@ -0,0 +1,12 @@
+package com.xin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by xsl on 2019/3/28.
+ *   config  interceptors, formatters, view controllers, and other features
+ */
+@Configuration
+public class SpringMVCConfig implements WebMvcConfigurer {
+}
